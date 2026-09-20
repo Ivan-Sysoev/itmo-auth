1e873639319488c12a12ec305fa8ce0123a751cc
Ivan-Sysoev <ivansysoev007@gmail.com>
2026-09-20 16:29:14 +0300

    fix(cache): skip http call on tomorrow's cache, add different return types for exceptions in get_cached_response

--- schedule.py
+++ schedule.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from types import ClassMethodDescriptorType
 from constants import (
     BASE_HEADERS,
     SCHEDULE_URL,
@@ -16,6 +17,7 @@ from datetime import (
 
 import json
 import requests
+import enum
 
 from itmo_auth import (
     ItmoAuth,
@@ -23,23 +25,33 @@ from itmo_auth import (
 
 auth = ItmoAuth()
 
+class CacheState(enum.Enum):
+    EXPIRED        = 0
+    DIFFERENT_DATE = 1
+
 def get_cached_response(target_date: date):
     if not SCHEDULE_CACHE_PATH.exists():
-        return None
+        return (None, CacheState.EXPIRED)
 
     with open(SCHEDULE_CACHE_PATH, 'r', encoding='utf-8') as ifile:
         json_data = json.load(ifile)
         cache_last_update_time = datetime.fromisoformat(json_data.get("cache_last_update_time"))
-        cache_expire_time = cache_last_update_time + SCHEDULE_EXPIRE_PERIOD
+        cache_target_date      = datetime.fromisoformat(json_data.get("cache_target_date"))
+        cache_expire_time      = cache_last_update_time + SCHEDULE_EXPIRE_PERIOD
 
-        if (target_date != cache_last_update_time.date() or datetime.now() >= cache_expire_time):
-            return None
-        
-        return json_data
+        if (target_date != cache_target_date.date()):
+            return (None, CacheState.DIFFERENT_DATE)
 
-def write_schedule_cache(json_data):
-    cache_last_update_time = datetime.now()
+        if (datetime.now() >= cache_expire_time):
+            return (None, CacheState.EXPIRED)
+
+        return (json_data, None)
+
+def write_schedule_cache(json_data, target_date: date):
+    cache_last_update_time              = datetime.now()
     json_data["cache_last_update_time"] = cache_last_update_time.isoformat()
+    json_data["cache_target_date"]      = target_date.isoformat()
+
     with open(SCHEDULE_CACHE_PATH, 'w', encoding='utf-8') as ofile:
         json.dump(json_data, ofile, ensure_ascii=False, indent=4)
     
@@ -86,7 +98,7 @@ def get_next_today_lesson(lessons):
             return lesson
     return None
 
-def format_lesson(lesson: dict | None) -> str:
+def format_lesson(lesson: dict | None, today: bool) -> str:
     if not lesson:
         return "There is no next lesson"
 
@@ -94,9 +106,9 @@ def format_lesson(lesson: dict | None) -> str:
     room = lesson["room"]
     start_time = lesson["start_time"].strftime("%H:%M")
 
-    return f"[ {name} ] {start_time} " + (f"(ауд. {room})" if room else "(Online)")
+    return ("[Завтра]" if not today else "") + f"[ {name} ] {start_time} " + (f"(ауд. {room})" if room else "(Online)")
 
-def get_next_lesson(lessons, today: bool) -> dict | None:
+def find_next_lesson(lessons, today: bool) -> dict | None:
     if not lessons:
         return None
     
@@ -110,27 +122,28 @@ def get_next_lesson(lessons, today: bool) -> dict | None:
     # Taking first tomorrow lesson
     return lessons[0]
 
-def get_schedule():
+def get_next_lesson():
     for target_date in (TODAY, TOMORROW):
-        update_cache = False
-        response = get_cached_response(target_date)
-        if not response:
-            update_cache = True
+        response, cache_state = get_cached_response(target_date)
+
+        if cache_state == CacheState.DIFFERENT_DATE:
+            continue
+
+        if cache_state == CacheState.EXPIRED:
             response = make_request(target_date)
 
         lessons = get_lessons(response)
-        next_lesson = get_next_lesson(lessons, target_date == TODAY)
+        next_lesson = find_next_lesson(lessons, target_date == TODAY)
 
         if next_lesson:
-            if update_cache:
-                write_schedule_cache(response)
-            return next_lesson
+            if cache_state == CacheState.EXPIRED:
+                write_schedule_cache(response, target_date)
+            return format_lesson(next_lesson, target_date == TODAY)
 
-    return None
+    return "There is no next lesson"
 
 def main():
-    lesson_output = get_schedule()
-    print(format_lesson(lesson_output))
+    print(get_next_lesson())
 
 if __name__ == "__main__":
     main()
